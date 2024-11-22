c4b7b65a9e281d2e414989cf2dcb269dc43a8730
BuidWebAgent <keajoker06@gmail.com>
2024-11-22 11:48:57 +0400

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -89,7 +89,7 @@ const sendMessage = async () => {
     chatInput.value = "";
 
     // Send user message to server and get response
-    const response = await fetch('https://93fb-2a00-f29-2d0-2b24-2d9d-c3db-c1b0-8e8c.ngrok-free.app/', {
+    const response = await fetch('https://a4d5-2001-8f8-1e6f-d7a-5452-a9c6-1158-7da8.ngrok-free.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
